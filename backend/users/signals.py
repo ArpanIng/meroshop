36202de267b7d34db288160ccdc9dda7c0b35be2
arpan <ingnamarpan@gmail.com>
2025-01-27 22:00:05 +0545

    Add: Signal to create cart for new registered users.

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -1,8 +1,14 @@
+import logging
+
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from carts.models import Cart
+
 from .models import Profile
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=get_user_model())
@@ -14,3 +20,10 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=get_user_model())
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+@receiver(post_save, sender=get_user_model())
+def create_user_cart(sender, instance, created, **kwargs):
+    if created:
+        Cart.objects.create(user=instance)
+        logger.info(f"Cart created for user {instance.email}.")
